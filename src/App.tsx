d9069eea997c9b1cf49090625c2031018deca25c
thegoliathgeek <dhanushdazz@gmail.com>
2023-06-06 00:16:12 +0530

    fix: routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "./components/Home";
+import { Container } from "@chakra-ui/react";
 
 function App() {
-  return <div>Home</div>;
+  return <Container h={"100vh"}>Hello World</Container>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, Container, extendTheme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/index.tsx";
+import NavBar from "./components/Navbar.tsx";
 
 const theme = extendTheme();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <NavBar />
+        <RouterProvider router={router} />
+      </div>
     </ChakraProvider>
   </React.StrictMode>
 );
